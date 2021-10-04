20c23e50bc559e980584687bd7cf3ffcd3be74ab
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2021-10-04 19:02:35 +0200

    fix: Error when deleting the firewall-rules

--- 02-open_vpn/clean-all.sh
+++ 02-open_vpn/clean-all.sh
@@ -2,4 +2,4 @@
 
 gcloud compute addresses delete uk-vpn-ip --region europe-west2
 gcloud compute instances delete vpn-instance --zone europe-west2-a
-gcloud compute firewall-rules create london-vpn-udp
+gcloud compute firewall-rules delete london-vpn-udp
