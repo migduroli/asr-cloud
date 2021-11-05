7100c97458daa795330eef9c0f3451ea7555f73a
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2021-11-05 18:10:16 +0100

    feat: Add template for assignment

---
+++ 10-assignment-template/main.py
@@ -0,0 +1,84 @@
+import os
+
+from google.cloud import storage
+from google.cloud import bigquery
+
+import pandas as pd
+
+
+DATASET = "transactions"
+TABLE = "records"
+TABLE_ID = f"{DATASET}.{TABLE}"
+
+
+def download_blob_as_dataframe(
+        bucket_name,
+        source_blob_name,
+        destination_file_name="transaction.csv"):
+    """Downloads a blob from a bucket and returns it as a DataFrame"""
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name=bucket_name)
+
+    blob = bucket.get_blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(
+        f"Downloaded storage object {source_blob_name} "
+        f"from bucket {bucket_name} to local file {destination_file_name}."
+    )
+
+    df = pd.read_csv(destination_file_name)
+
+    os.remove(destination_file_name)
+    return df
+
+
+def upload_to_bigquery(dataframe, destination_table_id):
+    """Uploads as dataframe into BigQuery"""
+
+    bq = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("ID", bigquery.enums.SqlTypeNames.STRING),
+            bigquery.SchemaField("AMOUNT", bigquery.enums.SqlTypeNames.FLOAT)
+        ],
+        write_disposition="WRITE_APPEND"
+    )
+
+    print(f"Data to be ingested:\n{dataframe}")
+
+    bq.load_table_from_dataframe(
+        dataframe=dataframe,
+        destination=destination_table_id,
+        job_config=job_config
+    )
+
+
+def run(event, context):
+    """The entrypoint of the Cloud Function"""
+    bucket_name = event["bucket"]
+    blob_name = event["name"]
+
+    print(f"[i] Bucket name: {bucket_name}")
+    print(f"[i] Filename storage path: {blob_name}")
+
+    df = download_blob_as_dataframe(
+        bucket_name=bucket_name,
+        source_blob_name=blob_name,
+    )
+
+    upload_to_bigquery(
+        dataframe=df,
+        destination_table_id=TABLE_ID
+    )
+
+
+if __name__ == "__main__":
+    event = {
+        "bucket": "asr-cloud-test-01",
+        "name": "transaction.csv"
+    }
+
+    run(event, None)
---
+++ 10-assignment-template/prepare_project.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## !!! IMPORTANT:
+## Uncomment the following line for the first running. After you've authenticated
+## once, you won't need to run this line anymore:
+# gcloud auth application-default login
+
+# Check if blob name exists, else it's created
+gsutil ls -b gs://asr-cloud-test-01 || gsutil mb -l ASIA gs://asr-cloud-test-01
+
+# Copy the example transaction in the desired bucket:
+gsutil cp transaction.csv gs://asr-cloud-test-01
+
+# Check if dataset transactions exists, else it's created
+bq ls transactions || bq mk -d transactions
+
+# Check if table records exists, else it's created
+bq ls transactions | grep records || bq mk --table transactions.records ID:STRING,AMOUNT:FLOAT
